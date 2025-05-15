27da90c3b8ed2d867c3c2cc9c7d74a82f93ed929
Enzo <tijoloenso@gmail.com>
2025-05-14 23:01:52 -0300

    Projeto criado

---
+++ src/test/java/io/github/enzopavani/productsapi/ProductsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.enzopavani.productsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
